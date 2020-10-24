f3ad1f79a042ff1f0eb51e99cdb371287cb2b6f2
VBiralo <mr.biralo@yandex.ru>
2020-10-25 01:11:34 +0300

    init: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrResult = [];
+    var str = str.split('');
+
+    if (str.length % 2) {
+        return false;
+    }
+
+    str.map((item) => {
+        bracketsConfig.map((config) => {
+            if (item === config[1] && arrResult[arrResult.length - 1] === config[0]) {
+                arrResult.pop();
+            } else if (item === config[0]) {
+                arrResult.push(item);
+            }
+        })
+    })
+
+    return arrResult.length === 0;
 }
